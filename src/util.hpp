177d281f1aa47c010cdf78392ca1347d32e26ff2
Eisenwave <janschultke@gmail.com>
2020-08-31 13:42:55 +0200

    add safe downcasts, document casts, rename memCast -> bitcast

--- src/util.hpp
+++ src/util.hpp
@@ -1,6 +1,12 @@
 #ifndef VXIO_UTIL_HPP
 #define VXIO_UTIL_HPP
+/*
+ * util.hpp
+ * --------
+ * Provides fundamental utilities such as isConstantEvaluated(), a safe downcast, etc.
+ */
 
+#include "assert.hpp"
 #include "builtin.hpp"
 #include "primitives.hpp"
 
@@ -29,13 +35,47 @@ constexpr bool isConstantEvaluated()
 #endif
 }
 
+/**
+ * @brief Performs a cast between types where the raw bits of one type are reinterpreted as that of another.
+ * This allows reinterpretation of e.g. an int as a float without undefined behavior.
+ * @param from the source type
+ * @return the casted result
+ */
 template <typename To, typename From>
-inline To memCast(const From &from)
+inline To bitcast(const From &from)
 {
     static_assert(sizeof(To) == sizeof(From));
     To result;
     std::memcpy(&result, &from, sizeof(To));
     return result;
+}
+
+/**
+ * @brief performs downcast of one reference to another.
+ * On debug builds, this will terminate if the cast fails.
+ */
+template <typename DERIVED, typename BASE, std::enable_if_t<std::is_reference_v<DERIVED>, int> = 0>
+constexpr DERIVED downcast(BASE &src) noexcept
+{
+    return VXIO_IF_DEBUG_ELSE(dynamic_cast<DERIVED>(src), static_cast<DERIVED>(src));
+}
+
+/**
+ * @brief Performs a downcast of one pointer to another.
+ * On debug builds, this will terminate if the cast fails.
+ * Downcasting a nullptr is always allowed.
+ */
+template <typename DERIVED, typename BASE, std::enable_if_t<std::is_pointer_v<DERIVED>, int> = 0>
+constexpr DERIVED downcast(BASE *src) noexcept
+{
+    if constexpr (build::DEBUG) {
+        DERIVED result = dynamic_cast<DERIVED>(src);
+        VXIO_DEBUG_ASSERT_CONSEQUENCE(src != nullptr, result != nullptr);
+        return result;
+    }
+    else {
+        return static_cast<DERIVED>(src);
+    }
 }
 
 /**
