8d4d9c996e41cf21c64c33d3041bab843188e7ef
Eisenwave <me@eisenwave.net>
2021-02-24 21:59:48 +0100

    enable support for GIF and BMP

--- src/format/png.cpp
+++ src/format/png.cpp
@@ -8,6 +8,8 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_ONLY_JPEG
 #define STBI_ONLY_PNG
+#define STBI_ONLY_BMP
+#define STBI_ONLY_GIF
 #include "voxelio/3rd_party/stb_image.h"
 
 namespace voxelio::png {
